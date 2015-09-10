0625dd8a2eee1a92488bc6b31b9ec11b766c2a34
William R Pearson <wrp@virginia.edu>
2015-09-09 22:10:34 -0400

    fixes for errors with --vdoms

--- scripts/ann_pfam_www_e.pl
+++ scripts/ann_pfam_www_e.pl
@@ -390,14 +390,15 @@ sub get_pfam_www {
   # each domain (but must also check for bounded-ness)
   # only add when 10% or more is missing and missing length > $min_nodom
 
-  if ($vdoms) {
+  if ($vdoms && scalar(@pf_domains)) {
       my @vpf_domains;
 
       my $curr_dom = $pf_domains[0];
+      my $length = $curr_dom->{model_length};
 
       my $prev_dom={end=>0, accession=>''};
       my $prev_dom_end = 0;
-      my $next_dom_start = $seq_length+1;
+      my $next_dom_start = $length+1;
 
       for (my $dom_ix=0; $dom_ix < scalar(@pf_domains); $dom_ix++ ) {
 	  $curr_dom = $pf_domains[$dom_ix];
