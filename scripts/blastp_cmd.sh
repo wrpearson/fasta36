464544732d7f98f8cc3132cd78e5fd35bccb1744
William R Pearson <wrp@virginia.edu>
2016-08-06 13:40:34 -0400

    mods to blast_cmd.sh

--- scripts/blastp_cmd.sh
+++ scripts/blastp_cmd.sh
@@ -16,6 +16,7 @@ done
 
 bl_asn=${OUTNAME}.asn
 bl0_out="$OUTNAME.html"
+blm_out="$OUTNAME.msa"
 blt_out="$OUTNAME.bl_tab"
 
 # echo "OUTFILE = ${OUTNAME}"
@@ -26,4 +27,5 @@ export BLAST_PATH="/seqprg/bin"
 $BLAST_PATH/blastp -outfmt 11 $cmd > $bl_asn
 $BLAST_PATH/blast_formatter -archive $bl_asn -outfmt 0 -html > $bl0_out
 $BLAST_PATH/blast_formatter -archive $bl_asn -outfmt '7 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore score btop'  > $blt_out
+$BLAST_PATH/blast_formatter -archive $bl_asn -outfmt 2  > $blm_out
 
