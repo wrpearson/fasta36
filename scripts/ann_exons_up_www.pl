a528bdbf0f92528d651b4b204d5d0b668a12cbcd
William R Pearson <wrp@virginia.edu>
2017-04-13 17:01:32 -0400

    added ann_exons_up_www.pl to annotate exon locations in proteins

--- scripts/ann_exons_up_www.pl
+++ scripts/ann_exons_up_www.pl
@@ -26,35 +26,26 @@
 # it must:
 # (1) read in the line
 # (2) parse it to get the acc
-# (3) return the tab delimited exon boundaries
-
+# (3) get exon information from EBI/Uniprot
+# (4) return the tab delimited exon boundaries
 
 use strict;
 
-use DBI;
 use Getopt::Long;
+use LWP::Simple;
+use LWP::UserAgent;
 use Pod::Usage;
+use JSON::Parse ':all';
 
 use vars qw($host $db $port $user $pass);
 
-my $hostname = `/bin/hostname`;
-
-($host, $db, $port, $user, $pass)  = ("wrpxdb.its.virginia.edu", "uniprot", 0, "web_user", "fasta_www");
-
-my ($auto_reg,$rpd2_fams, $neg_doms, $lav, $no_doms, $pf_acc, $shelp, $help) = (0, 0, 0, 0,0, 0,0,0);
-my ($min_nodom) = (10);
+my ($lav, $shelp, $help) = (0, 0,0);
 
 my $color_sep_str = " :";
 $color_sep_str = '~';
 
 GetOptions(
-    "host=s" => \$host,
-    "db=s" => \$db,
-    "user=s" => \$user,
-    "password=s" => \$pass,
-    "port=i" => \$port,
     "lav" => \$lav,
-    "min_nodom=i" => \$min_nodom,
     "h|?" => \$shelp,
     "help" => \$help,
     );
@@ -63,34 +54,17 @@ pod2usage(1) if $shelp;
 pod2usage(exitstatus => 0, verbose => 2) if $help;
 pod2usage(1) unless (@ARGV || -p STDIN || -f STDIN);
 
-my $connect = "dbi:mysql(AutoCommit=>1,RaiseError=>1):database=$db";
-$connect .= ";host=$host" if $host;
-$connect .= ";port=$port" if $port;
-
-my $dbh = DBI->connect($connect,
-		       $user,
-		       $pass
-		      ) or die $DBI::errstr;
-
-my %annot_types = ();
 my %domains = (NODOM=>0);
+my @domain_list = (0);
 my $domain_cnt = 0;
 
-my $get_annot_sub = \&get_ensembl_exons;
-
-my $get_exons_acc = $dbh->prepare(<<EOSQL);
-
-SELECT ex_num, ex_p_start as seq_start, ex_p_end as seq_end
-FROM ens_exons
-JOIN ens2up USING(ensp)
-WHERE  acc=?
-ORDER BY seq_start
-
-EOSQL
-
-my $get_annots_sql = $get_exons_acc;
-
 my ($tmp, $gi, $sdb, $acc, $id, $use_acc);
+
+my $get_annot_sub = \&get_up_www_exons;
+
+my $ua = LWP::UserAgent->new(ssl_opts=>{verify_hostname => 0});
+my $uniprot_url = 'https://www.ebi.ac.uk/proteins/api/coordinates/';
+
 
 # get the query
 my ($query, $seq_len) = @ARGV;
@@ -116,9 +90,6 @@ else {
 for my $seq_annot (@annots) {
   print ">",$seq_annot->{seq_info},"\n";
   for my $annot (@{$seq_annot->{list}}) {
-    if (!$lav && defined($domains{$annot->[-1]})) {
-      $annot->[-1] .= $color_sep_str.$domains{$annot->[-1]};
-    }
     print join("\t",@$annot),"\n";
   }
 }
@@ -130,12 +101,9 @@ sub show_annots {
 
   my ($annot_line, $seq_len) = split(/\s+/,$query_len);
 
-  my $pfamA_acc;
-
   my %annot_data = (seq_info=>$annot_line);
 
   $use_acc = 1;
-  $get_annots_sql = $get_exons_acc;
 
   if ($annot_line =~ m/^gi\|/) {
     ($tmp, $gi, $sdb, $acc, $id) = split(/\|/,$annot_line);
@@ -145,51 +113,66 @@ sub show_annots {
   }
 
   $acc =~ s/\.\d+$//;
-  $get_annots_sql->execute($acc);
 
-  $annot_data{list} = $get_annot_sub->($get_annots_sql, $seq_len);
+  my $exon_json = get_https($uniprot_url.$acc);
+
+  unless (!$exon_json || $exon_json =~ m/errorMessage/ || $exon_json =~ m/Can not find/) {
+    $annot_data{list} = parse_json_up_exons($exon_json);
+  }
 
   return \%annot_data;
 }
 
-sub get_ensembl_exons {
-  my ($get_annots, $seq_length) = @_;
+sub parse_json_up_exons {
+  my ($exon_json) = @_;
 
   my @exons = ();
 
-  # get the list of domains, sorted by start
-  while ( my $row_href = $get_annots->fetchrow_hashref()) {
+  my $acc_exons = parse_json($exon_json);
 
-    $row_href->{info} = "exon_".$row_href->{ex_num};
-    push @exons, $row_href
+  my $exon_num = 1;
+  for my $exon ( @{$acc_exons->{'gnCoordinate'}[0]{'genomicLocation'}{'exon'}} ) {
+    my ($p_begin, $p_end) = ($exon->{'proteinLocation'}{'begin'}{'position'},$exon->{'proteinLocation'}{'end'}{'position'});
+    if ($p_end >= $p_begin) {
+      push @exons, {
+		    info=>"exon_".$exon_num.$color_sep_str.$exon_num,
+		    seq_start=>$p_begin,
+		    seq_end=>$p_end,
+		   };
+      $exon_num++;
+    }
   }
 
   # check for domain overlap, and resolve check for domain overlap
   # (possibly more than 2 domains), choosing the domain with the best
   # evalue
 
-  my @feats = ();
+  my @ex_feats = ();
 
   for my $d_ref (@exons) {
     if ($lav) {
-      push @feats, [$d_ref->{seq_start}, $d_ref->{seq_end}, $d_ref->{info}];
+      push @ex_feats, [$d_ref->{seq_start}, $d_ref->{seq_end}, $d_ref->{info}];
     }
     else {
-      push @feats, [$d_ref->{seq_start}, '-', $d_ref->{seq_end},  $d_ref->{info} ];
-#      push @feats, [$d_ref->{seq_end}, ']', '-', ""];
+      push @ex_feats, [$d_ref->{seq_start}, '-', $d_ref->{seq_end},  $d_ref->{info} ];
     }
-
   }
-
-  return \@feats;
+  return \@ex_feats;
 }
 
-# domain name takes a uniprot domain label, removes comments ( ;
-# truncated) and numbers and returns a canonical form. Thus:
-# Cortactin 6.
-# Cortactin 7; truncated.
-# becomes "Cortactin"
-#
+sub get_https {
+  my ($url) = @_;
+
+  my $result = "";
+  my $response = $ua->get($url);
+
+  if ($response->is_success) {
+    $result = $response->decoded_content;
+  } else {
+    $result = '';
+  }
+  return $result;
+}
 
 sub domain_name {
 
@@ -208,45 +191,28 @@ __END__
 
 =head1 NAME
 
-ann_feats.pl
+ann_exons_up_www.pl
 
 =head1 SYNOPSIS
 
- ann_pfam.pl --neg-doms  'sp|P09488|GSTM1_NUMAN' | accession.file
+ ann_exons_up_www.pl 'sp|P09488|GSTM1_HUMAN' | accession.file
 
 =head1 OPTIONS
 
  -h	short help
  --help include description
- --neg-doms,  -- report domains between annotated domains as NODOM
-                 (also --neg, --neg_doms)
- --min_nodom=10  -- minimum length between domains for NODOM
-
- --host, --user, --password, --port --db -- info for mysql database
+ --lav  produce lav2plt.pl annotation format
 
 =head1 DESCRIPTION
 
-C<ann_pfam.pl> extracts domain information from a msyql
-database.  Currently, the program works with database sequence
-descriptions in one of two formats:
-
- >pf26|649|O94823|AT10B_HUMAN -- RPD2_seqs
-
-(pf26 databases have auto_pfamseq in the second field) and
-
- >gi|1705556|sp|P54670.1|CAF1_DICDI
-
-C<ann_pfam.pl> uses the C<pfamA_reg_full_significant>, C<pfamseq>,
-and C<pfamA> tables of the C<pfam> database to extract domain
-information on a protein.  For proteins that have multiple domains
-associated with the same overlapping region (domains overlap by more
-than 1/3 of the domain length), C<auto_pfam.pl> selects the domain
-annotation with the best C<domain_evalue_score>.  When domains overlap
-by less than 1/3 of the domain length, they are shortened to remove
-the overlap.
+C<ann_exons_up_www.pl> extracts exon coordinates for proteins using
+the EBI Proteins REST API described here:
+C<https://www.ebi.ac.uk/proteins/api/doc/#coordinatesApi>.  Exon
+intron boundaries, in protein coordinates, are available for Uniprot
+proteins with Ensembl entries.
 
 C<ann_pfam.pl> is designed to be used by the B<FASTA> programs with
-the C<-V \!ann_pfam.pl> or C<-V "\!ann_pfam.pl --neg"> option.
+the C<-V \!ann_exons_up_www.pl> or C<-V "q\!ann_exons_up_www.plg"> option.
 
 =head1 AUTHOR
 
