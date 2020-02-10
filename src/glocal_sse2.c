492e651eb71e77ff273248371d53781f7d998292
William R Pearson <wrp@virginia.edu>
2020-02-10 09:20:43 -0500

    modify copyright notice for Farrar .sse2 implementations

--- src/glocal_sse2.c
+++ src/glocal_sse2.c
@@ -1,14 +1,11 @@
 /******************************************************************
-  Copyright 2010 by Michael Farrar.  All rights reserved.
-  This program may not be sold or incorporated into a commercial product,
-  in whole or in part, without written consent of Michael Farrar.  For 
-  further information regarding permission for use or reproduction, please 
-  contact: Michael Farrar at farrar.michael@gmail.com.
+  Copyright 2010 Michael Farrar.  
+  Freely distributed under the BSD open source license.
+  See the ../COPYRIGHT.sse2 file for details.
 *******************************************************************/
 
 /*
   Written by Michael Farrar, 2010.
-  Please send bug reports and/or suggestions to farrar.michael@gmail.com.
 */
 
 #include <stdio.h>
--- src/smith_waterman_sse2.c
+++ src/smith_waterman_sse2.c
@@ -1,14 +1,12 @@
 /******************************************************************
-  Copyright 2006 by Michael Farrar.  All rights reserved.
-  This program may not be sold or incorporated into a commercial product,
-  in whole or in part, without written consent of Michael Farrar.  For 
-  further information regarding permission for use or reproduction, please 
-  contact: Michael Farrar at farrar.michael@gmail.com.
+  Copyright 2006 Michael Farrar.  
+  Freely distributed under the BSD open source license.
+  See the ../COPYRIGHT.sse2 file for details.
 *******************************************************************/
 
 /*
   Written by Michael Farrar, 2006.
-  Please send bug reports and/or suggestions to farrar.michael@gmail.com.
+  For information, contact Sean Eddy (sean@eddylab.org).
 */
 
 #include <stdio.h>
